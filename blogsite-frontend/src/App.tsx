45f3e4187d05ada4d51559fad78fadbd9b69e308
Szolnoki Abel <szolnokiabi25@gmail.com>
2026-06-01 21:42:36 +0200

    primereact init

--- blogsite-frontend/src/App.tsx
+++ blogsite-frontend/src/App.tsx
@@ -2,15 +2,19 @@ import "./App.css";
 import { UserContext } from "./contexts/user-context";
 import Router from "./router";
 import type User from "./types/user-type";
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-dark-cyan/theme.css";
+import "primeicons/primeicons.css";
 
 function App() {
   const user: User = { id: 0, loggedIn: false };
-
   return (
     // UserContext feltöltése értékkel, a Router alkalmazása
-    <UserContext.Provider value={user}>
-      <Router />
-    </UserContext.Provider>
+    <PrimeReactProvider>
+      <UserContext.Provider value={user}>
+        <Router />
+      </UserContext.Provider>
+    </PrimeReactProvider>
   );
 }
 
